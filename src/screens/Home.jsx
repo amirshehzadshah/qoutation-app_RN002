a5bbe08a38a7e4585f4ec39a74024673dbdd5a73
Amir Shehzad Shah <140312408+amirshehzadshah@users.noreply.github.com>
2025-01-08 22:24:03 +0500

    design home screen with custom font and image

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,20 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { useFonts, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+const { width, height } = Dimensions.get('window');
 
 export default function Home() {
+
+  const [fontsLoaded] = useFonts({ Poppins_600SemiBold })
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Home Page!</Text>
+    <View style={styles.Container}>
+      <Text style={styles.ScreenText}>Welcome to</Text>
+      <Image
+        source={require('../../assets/orions_v.png')}
+        style={[styles.ScreenImage, { width: width, height: height * 0.2 }]}
+        resizeMode="contain"
+      />
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  Container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  ScreenText: {
+    fontSize: 48,
+    fontFamily: 'Poppins_600SemiBold',
+    color: '#333333',
   },
 });
